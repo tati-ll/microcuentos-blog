c343fd405477bd8605baf88135d9492f781216b8
Tatiana Llana <tjllana@uc.cl>
2023-08-09 18:22:25 -0400

    Se creo modelo Post, y se agregaron instancias a BD para ejemplo

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    subtitulo = models.TextField()
+    cuerpo = models.TextField()
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    fecha = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.titulo
