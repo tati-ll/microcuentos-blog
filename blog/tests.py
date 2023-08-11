72bd8c62ed4a706a3054f82087f994c3c38548fc
Tatiana Llana <tjllana@uc.cl>
2023-08-11 15:43:53 -0400

    Pruebas unitarias para modelo Post de la app 'blog' y para modelo Usuario de la app 'perfiles' OK

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from blog.models import Post
+
+class PostTest(TestCase):
+    """En esta clase están todas las pruebas del modelo Post"""
+
+    def setUp(self):
+        # Crear un usuario para usar en las pruebas
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        
+        # Crear un objeto Post para usar en las pruebas
+        self.post = Post.objects.create(
+            titulo="Titulo",
+            subtitulo="subtitulo",
+            cuerpo="Texto, texto, texto",
+            autor=self.user
+        )
+
+        # Comprobar que el post fue creado y la data se guardó correctamente
+        self.assertIsNotNone(self.post)
+        self.assertEqual(self.post.titulo, "Titulo")
+        self.assertEqual(self.post.subtitulo, "subtitulo")
+        self.assertEqual(self.post.cuerpo, "Texto, texto, texto")
+        self.assertEqual(self.post.autor, self.user)
+
+    def test_editar_post(self):
+        # Editar el objeto Post
+        self.post.titulo = "Nuevo Titulo"
+        self.post.save()
+
+        # Comprobar que los cambios se guardaron correctamente
+        self.assertEqual(self.post.titulo, "Nuevo Titulo")
+
+    def test_eliminar_post(self):
+        # Eliminar el objeto Post
+        self.post.delete()
+
+        # Comprobar que el objeto se eliminó correctamente
+        self.assertEqual(Post.objects.count(), 0)
+
--- perfiles/tests.py
+++ perfiles/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
 
-# Create your tests here.
+from perfiles.models import Usuario
+
+class UsuarioTest(TestCase):
+    def test_crear_usuario(self):
+        # Crear un nuevo usuario
+        usuario = Usuario.objects.create_user(username='nuevo_usuario', password='password123')
+        
+        # Comprobar que el usuario fue creado correctamente
+        self.assertEqual(usuario.username, 'nuevo_usuario')
+        self.assertTrue(usuario.check_password('password123'))
+    
+    def test_editar_usuario(self):
+        # Crear un usuario de prueba para editar
+        usuario = Usuario.objects.create_user(username='usuario_a_editar', password='password123')
+        
+        # Editar los datos del usuario
+        usuario.first_name = 'Nuevo Nombre'
+        usuario.last_name = 'Nuevo Apellido'
+        usuario.save()
+        
+        # Obtener el usuario actualizado desde la base de datos
+        usuario_actualizado = Usuario.objects.get(username='usuario_a_editar')
+        
+        # Comprobar que los datos se actualizaron correctamente
+        self.assertEqual(usuario_actualizado.first_name, 'Nuevo Nombre')
+        self.assertEqual(usuario_actualizado.last_name, 'Nuevo Apellido')
+    
+    def test_eliminar_usuario(self):
+        # Crear un usuario para eliminar
+        usuario = Usuario.objects.create_user(username='usuario_a_eliminar', password='password123')
+        
+        # Eliminar el usuario
+        usuario.delete()
+        
+        # Comprobar que el usuario fue eliminado correctamente
+        with self.assertRaises(Usuario.DoesNotExist):
+            Usuario.objects.get(username='usuario_a_eliminar')
+
+
